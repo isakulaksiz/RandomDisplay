04b2c58bb08662a79e5c7427bdbf31de63ee2770
İsa Kulaksız <isakulaksiz@outlook.com>
2022-08-12 20:15:48 +0300

    Initial Commit

---
+++ RandomDisplay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomDisplay
+//
+//  Created by Isa Kulaksız on 12.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
