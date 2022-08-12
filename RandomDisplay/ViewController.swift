4374bd89d1dad330ad5c6acbaa283a782edc9f0c
İsa Kulaksız <isakulaksiz@outlook.com>
2022-08-12 20:40:12 +0300

    added random image by url

--- RandomDisplay/ViewController.swift
+++ RandomDisplay/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
+        getRandomPhoto()
     }
 
+    func getRandomPhoto(){
+        let urlString = "https://source.unsplash.com/random/600x600"
+        
+        let url = URL(string: urlString)!
+        
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        
+        imageView.image = UIImage(data: data)
+    }
 
 }
 
